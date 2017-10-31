6d86d2a4a80f7ebefcff179b6d546f1e6e87c62f
hanbollar <hbollar@seas.upenn.edu>
2017-10-31 02:58:25 -0400

    switched to 3x3x3x... indexing scheme for N and N' for weight vals

--- src/simulation/AttributeTransfer.h
+++ src/simulation/AttributeTransfer.h
@@ -11,7 +11,7 @@ struct AttributeTransfer {
 	class WeightVals {
 
 	public:
-		static constexpr unsigned int kLen = 3 + (Dimension - 1) * 2;
+		static constexpr unsigned int kLen = 3 * Dimension;
 		Eigen::Array<Eigen::Vector3f, kLen, 1> N;
 		Eigen::Array<Eigen::Vector3f, kLen, 1> N_deriv;
 
@@ -28,17 +28,17 @@ struct AttributeTransfer {
 		//  Z -Y
 
 		// N:
-		//   E F
-		// A B C
-		// G D
+		//		   F	  G
+		// A B C   E	 H
+		//		   D	I
 		// N':
-		//   L M
-		// H I J
-		// N K
+		//		   O	  P
+		// J K L   N	 Q
+		//		   M	R
 
 		// ordering in 2d array
-		// ABC DE FG -- for w
-		// HIJ KL MN -- for w_deriv
+		// ABC DEF GHI -- for N
+		// JKL MNO PQR -- for N_deriv
 
 	private:
 
@@ -81,15 +81,6 @@ struct AttributeTransfer {
 		} 
 
 		static Eigen::Array<T, Dimension, 1> indexN(Eigen::Array<T, Dimension, 1> xp, Eigen::Array<T, Dimension, 1> xi, float cellSize) {
-			if (i < 3) {
-				loc[0] += static_cast<float>(-cellSize + i * cellSize);
-			}
-			else {
-				// index for arb sized grid
-				int index = (i - 3) / 2 + 1;
-				loc[index] += ((i + 1) % 2 == 0) ? -cellSize : cellSize;
-			}
-
 			Eigen::Array<T, Dimension, 1> fixedParam = ((xp - xi) / cellSize);
 			Eigen::Array<T, Dimension, 1> index = Eigen::Array<T, Dimension, 1>();
 			index.fill(0.f);
@@ -103,36 +94,48 @@ struct AttributeTransfer {
 					return -1;
 				}
 
-				if (i == 0) {
-					// x val has 3 components all others have 2
-					loc[i] = (aVal > 1) ? 0 : ((val >= 0) ? 1 : 2) ;
-				}
-				else {
-					float offset = (i - 1) * 2 + 3;
-					loc[i] = (aVal > 1) ? offset : offset + 1;
-				}
+				int offset = i * 3;
+				offset += (aVal > 1) ? 0
+									: (aVal < 1 && aval > .5f * sqrt(2)) ? 1
+																		 : 2;
+				loc[i] = offset;
 			}
 
 			return index;
 		}
 
-		// calculating a weight property [wip or gradwip] with reference to a specific grid location
-		Eigen::Array<T, Dimension, 1> calcWeightProp(Eigen::Array<T, Dimension, 1> xp, Eigen::Array<T, Dimension, 1> xi, float cellSize, bool funcOrDeriv) {
-			Eigen::Array<T, Dimension, 1> index = indexN(xp, xi, cellSize);
-
-			Eigen::Array<T, Dimension, 1>  calc = Eigen::Array<T, Dimension, 1>();
-			calc.fill(1.f);
+		static Eigen::Array<T, Dimension, 2> indexN_deriv(Eigen::Array<T, Dimension, 1> xp, Eigen::Array<T, Dimension, 1> xi, float cellSize) {
+			Eigen::Array<T, Dimension, 1> fixedParam = ((xp - xi) / cellSize);
+			Eigen::Array<T, Dimension, 2> indexRange = Eigen::Array<T, Dimension, 2>();
+			indexRange.fill(0.f);
 
 			for (int i = 0; i < Dimension; i++) {
-				calc *= (funcOrDeriv) ? N[index[i]] : N_deriv[index[i]];
+
+				float val = fixedParam[i];
+				float aVal = abs(fixedParam[i]);
+				if (val > 2 || val < -1) {
+					// outside of checking region
+					throw;
+				}
+
+				//if (i == 0) {
+				//	// x val has 3 components all others have 2
+				//	loc[i][0] = (aVal > 1) ? 0 : 1;
+				//	loc[i][1] = (aVal > 1) ? 1 : 2;
+				//}
+				//else {
+				//	float offset = (i - 1) * 2 + 3;
+				//	loc[i][0] = (aVal > 1) ? offset : ;/// rewrite everything base 3
+				//	loc[i][0] = (aVal > 1) ? offset : offset + 1;
+				//} TODOOOOOO
 			}
 
-			return calc;
+			return index;
 		}
 
 	public:
 
-		// calc weights for curr particle positions
+		// calc N and N' values for curr particle positions
 		void fill_Weights(Eigen::Array<T, Dimension, 1> xp, float cellSize) {
 
 			auto intCast = Eigen::Array<T, Dimension, 1>();
@@ -144,13 +147,8 @@ struct AttributeTransfer {
 			for (unsigned int i = 0; i < kLen; i++) {
 				Eigen::Array<T, Dimension, 1> loc = intCast;
 
-				if (i < 3) {
-					loc[0] += static_cast<float>(-cellSize + i * cellSize);
-				} else {
-					// index for arb sized grid
-					int index = (i - 3) / 2 + 1;
-					loc[index] += ((i + 1) % 2 == 0) ? -cellSize : cellSize;
-				}
+				int index = i / 3 + i % 3;
+				loc[index] += -1.f * cellSize + (i % 3) * cellSize;
 
 				auto Nval = calcN(xp, loc, cellSize);
 				auto N_derivVal = calcN_deriv_V(xp, loc, cellSize);
@@ -162,12 +160,30 @@ struct AttributeTransfer {
 
 		// calculating Wip with reference to a specific cell xi
 		Eigen::Array<T, Dimension, 1> calcWip(Eigen::Array<T, Dimension, 1> xp, Eigen::Array<T, Dimension, 1> xi, float cellSize) {
-			return calcWeightProp(xp, xi, cellSize, true);
+			Eigen::Array<T, Dimension, 1> index = indexN(xp, xi, cellSize);
+
+			Eigen::Array<T, Dimension, 1>  calc = Eigen::Array<T, Dimension, Dimension>();
+			calc.fill(1.f);
+
+			for (int i = 0; i < Dimension; i++) {
+				calc *= N[index[i]];
+			}
+
+			return calc;
 		}
 
 		// calculating gradWip with reference to a specific cell xi
 		Eigen::Array<T, Dimension, 1> calcGradWip(Eigen::Array<T, Dimension, 1> xp, float cellSize) {
-			return calcWeightProp(xp, xi, cellSize, false);
+			Eigen::Array<T, Dimension, 1> index = indexN(xp, xi, cellSize);
+
+			Eigen::Array<T, Dimension, 1>  calc = Eigen::Array<T, Dimension, 1>();
+			calc.fill(1.f); //---------------------------TODOTODOTODOTODOTODO
+
+			for (int i = 0; i < Dimension; i++) {
+				calc[i] = (funcOrDeriv) ? N[index[i]] : N_deriv[index[i]];
+			}
+
+			return calc;
 		}
 
 	};
